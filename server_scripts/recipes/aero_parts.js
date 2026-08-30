b26391e3e7afd8808966c5d57124586091b70611
Spudacious <145444678+Milo-Cat@users.noreply.github.com>
2026-08-30 01:34:09 +0100

    Recipe for portable engine

--- server_scripts/recipes/aero_parts.js
+++ server_scripts/recipes/aero_parts.js
@@ -2,6 +2,21 @@ ServerEvents.recipes(event => {
 
     event.replaceInput({output:'addon_gancho:grapple_swing'},'simulated:gyroscopic_mechanism','immersiveengineering:component_iron');
 
+    
+	event.shaped(
+		Item.of('simulated:red_portable_engine', 1),
+		[
+			' T ',
+			'PMP',
+			' F '
+		],
+		{
+			T: 'create:iron_sheet',
+			P: 'createdieselgenerators:engine_piston',
+            M: 'immersiveengineering:component_iron',
+            F: 'minecraft:blast_furnace'
+		}
+	);
 
 }
 )
