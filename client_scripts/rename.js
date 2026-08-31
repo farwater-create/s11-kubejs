e218635872b867bbd1026a33808562908ad73abb
Milo-Cat <Wlspender05@gmail.com>
2026-08-31 21:27:34 +0100

    Renamed brazil legends mobs

---
+++ client_scripts/rename.js
@@ -0,0 +1,8 @@
+ClientEvents.lang('en_us', event => {
+
+    event.renameEntity('brazil_legends:capelobo', 'Crooked Wolf');
+    event.renameEntity('brazil_legends:mapinguari', 'Jungle Cyclops');
+    event.renameEntity('brazil_legends:mapinguari', 'Cursed Cyclops');
+    event.renameEntity('brazil_legends:cuca', 'Child Eater');
+
+})
