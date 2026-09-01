653738226780a1be9b8c22930150cee5e8b8dece
Milo-Cat <Wlspender05@gmail.com>
2026-09-01 22:10:27 +0100

    duplicate names

--- client_scripts/rename.js
+++ client_scripts/rename.js
@@ -2,7 +2,7 @@ ClientEvents.lang('en_us', event => {
 
     event.renameEntity('brazil_legends:capelobo', 'Crooked Wolf');
     event.renameEntity('brazil_legends:mapinguari', 'Jungle Cyclops');
-    event.renameEntity('brazil_legends:mapinguari', 'Cursed Cyclops');
+    event.renameEntity('brazil_legends:mapinguary', 'Cursed Cyclops');
     event.renameEntity('brazil_legends:cuca', 'Child Eater');
 
 })
