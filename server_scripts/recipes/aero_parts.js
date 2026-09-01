ec14078cdf2ce534d97f81dc4dec84621846857e
Milo-Cat <Wlspender05@gmail.com>
2026-09-01 01:35:27 +0100

    format

--- server_scripts/recipes/aero_parts.js
+++ server_scripts/recipes/aero_parts.js
@@ -1,50 +1,50 @@
 ServerEvents.recipes(event => {
 
-    event.replaceInput({output:'addon_gancho:grapple_swing'},'simulated:gyroscopic_mechanism','immersiveengineering:component_iron');
+        event.replaceInput({output: 'addon_gancho:grapple_swing'}, 'simulated:gyroscopic_mechanism', 'immersiveengineering:component_iron');
 
-    
-	event.shaped(
-		Item.of('simulated:red_portable_engine', 1),
-		[
-			' T ',
-			'PMP',
-			' F '
-		],
-		{
-			T: 'create:iron_sheet',
-			P: 'createdieselgenerators:engine_piston',
-            M: 'immersiveengineering:component_iron',
-            F: 'minecraft:blast_furnace'
-		}
-	);
 
-	event.remove({ id: 'simulated:rope_connector' })
-	event.shaped(
-		Item.of('simulated:rope_connector'),
-		[
-			'P',
-			'B'
-		],
-		{
-			P: '#c:plates/iron',
-			B: '#simulated:handles'
-		}
-	)
+        event.shaped(
+            Item.of('simulated:red_portable_engine', 1),
+            [
+                ' T ',
+                'PMP',
+                ' F '
+            ],
+            {
+                T: 'create:iron_sheet',
+                P: 'createdieselgenerators:engine_piston',
+                M: 'immersiveengineering:component_iron',
+                F: 'minecraft:blast_furnace'
+            }
+        );
 
-	event.remove({ id: 'simulated:rope_winch' })
-	event.shaped(
-		Item.of('simulated:rope_winch'),
-		[
-			'P',
-			'S',
-			'B'
-		],
-		{
-			P: '#c:plates/iron',
-			S: 'create:shaft',
-			B: '#simulated:handles'
-		}
-	)
+        event.remove({id: 'simulated:rope_connector'})
+        event.shaped(
+            Item.of('simulated:rope_connector'),
+            [
+                'P',
+                'B'
+            ],
+            {
+                P: '#c:plates/iron',
+                B: '#simulated:handles'
+            }
+        )
+
+        event.remove({id: 'simulated:rope_winch'})
+        event.shaped(
+            Item.of('simulated:rope_winch'),
+            [
+                'P',
+                'S',
+                'B'
+            ],
+            {
+                P: '#c:plates/iron',
+                S: 'create:shaft',
+                B: '#simulated:handles'
+            }
+        )
 
 }
 )
