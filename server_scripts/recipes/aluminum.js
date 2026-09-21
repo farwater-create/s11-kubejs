bc89f1457c2464a74583864b950ba9dd3a925a63
ULSTICK <Rafi.kamal93@yahoo.com>
2026-09-21 20:09:52 +0700

    Update Aluminum processing (still disabled)

--- server_scripts/recipes/aluminum.js
+++ server_scripts/recipes/aluminum.js
@@ -2,8 +2,14 @@ ServerEvents.recipes(event => {
     
     event.remove({ output: 'immersiveengineering:ingot_aluminum', type: 'minecraft:smelting'})
     event.remove({ output: 'immersiveengineering:ingot_aluminum', type: 'minecraft:blasting' })
+    event.remove({ output: 'immersiveengineering:ingot_aluminum', type: 'immersiveengineering:arc_furnace' })
     event.remove({ output: 'advancedweather:aluminum_ingot', type: 'minecraft:blasting'})
+    event.remove({ id: 'advancedweather:compat/aluminum_ingot_from_mixing'})
+    event.remove({ id: 'immersiveengineering:crafting/hammercrushing_aluminum'})
     event.remove({ id: 'immersiveengineering:crafting/raw_hammercrushing_aluminum'})
+    event.remove({ id: 'immersiveengineering:crusher/ore_aluminum'})
+    event.remove({ id: 'immersiveengineering:crusher/raw_ore_aluminum'})
+    event.remove({ id: 'immersiveengineering:crusher/raw_block_aluminum'})
     event.remove({ id: 'create:splashing/immersiveengineering/crushed_raw_aluminum'})
     event.remove({ id: 'create:crushing/aluminum_ore'})
 
@@ -36,19 +42,47 @@ ServerEvents.recipes(event => {
         ],
         Ingredient.of('create:crushed_raw_aluminum')
     )
+    event.custom({
+        type: 'immersiveengineering:crusher',
+        energy: 6000,
+        input: {
+          tag: 'c:raw_materials/aluminum'
+        },
+        result: {
+          item: 'advancedweather:alumina',
+          count: 3
+        },
+        secondaries: [
+          {
+            chance: 0.25,
+            output: {
+              item: 'advancedweather:alumina'
+            }
+          }
+        ]
+    })
 
     event.custom({
-        type: 'immersiveengineering:blast_furnace',
+        type: "immersiveengineering:arc_furnace",
+        additives: [],
+        energy: 51200,
         input: {
             item: 'advancedweather:alumina_carbon_mix'
         },
-        result: {
-            tag: 'c:ingots/aluminum'
-        },
-        slag: {
-            tag: 'c:slag'
-        },
-        time: 1800
+        results: [
+            {
+                tag: 'c:ingots/aluminum'
+            }
+        ],
+        time: 600
     })
+
+    event.recipes.create.crushing(
+        [
+            Item.of('immersiveengineering:dust_aluminum')
+        ],
+        Ingredient.of('#c:ingots/aluminum')
+    )
+
     */
 })
