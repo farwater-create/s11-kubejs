b3f1063669d43ca6872716299d4b568128a482c9
ULSTICK <Rafi.kamal93@yahoo.com>
2026-09-23 21:16:01 +0700

    More AE2 Changes (they're still disabled dw)

--- server_scripts/recipes/ae2.js
+++ server_scripts/recipes/ae2.js
@@ -1,7 +1,10 @@
 ServerEvents.recipes(event => {
-    event.replaceInput({ output:'ae2:inscriber' }, 'minecraft:copper_ingot', 'kubejs:redstone_circuit')
-    event.replaceInput({ mod: 'ae2', input: 'minecraft:iron_ingot' }, 'minecraft:iron_ingot', '#c:ingots/aluminum')
-    event.replaceInput({ mod: 'ae2', input: '#c:ingots/iron', }, '#c:ingots/iron', '#c:ingots/aluminum')
+    event.replaceInput({ mod: 'ae2', input: 'minecraft:iron_ingot' }, 'minecraft:iron_ingot', '#c:plates/aluminum')
+    
+    event.stonecutting('ae2:calculation_processor_press', 'cataclysm:witherite_ingot')
+    event.stonecutting('ae2:engineering_processor_press', 'cataclysm:witherite_ingot')
+    event.stonecutting('ae2:logic_processor_press', 'cataclysm:witherite_ingot')
+    event.stonecutting('ae2:silicon_press', 'cataclysm:witherite_ingot')
 
     //region assemblies
     let transitional
@@ -134,10 +137,69 @@ ServerEvents.recipes(event => {
             Fluid.of('minecraft:water', 250)
         ]
     )
+
+    //region Certus Quartz
+
     event.recipes.create.crushing(['ae2:flawless_budding_quartz', CreateItem.of('ae2:certus_quartz_crystal', 0.5), CreateItem.of('ae2:certus_quartz_crystal', 0.25)], 'ae2:flawless_budding_quartz').processingTime(600)
 
     event.recipes.create.crushing(['ae2:chipped_budding_quartz', Item.of('ae2:certus_quartz_crystal', 2), CreateItem.of('ae2:certus_quartz_crystal', 0.5), CreateItem.of('ae2:certus_quartz_crystal', 0.25)], 'ae2:flawed_budding_quartz').processingTime(1200)
     event.recipes.create.crushing(['ae2:damaged_budding_quartz', Item.of('ae2:certus_quartz_crystal', 2), CreateItem.of('ae2:certus_quartz_crystal', 0.5), CreateItem.of('ae2:certus_quartz_crystal', 0.25)], 'ae2:chipped_budding_quartz').processingTime(1200)
     event.recipes.create.crushing([Item.of('ae2:certus_quartz_crystal', 6), CreateItem.of('ae2:certus_quartz_crystal', 0.5), CreateItem.of('ae2:certus_quartz_crystal', 0.25)], 'ae2:chipped_budding_quartz').processingTime(1200)
+
+    //endregion Certus Quartz
+
+    //region AE Machines
+
+    event.remove({ id: 'ae2:network/blocks/crystal_processing_charger' })
+    event.shaped(
+        Item.of('ae2:charger'),
+        [
+            'APA',
+            'A  ',
+            'ACA'
+        ],
+        {
+            A: '#c:plates/aluminum',
+            P: 'immersiveengineering:wirecoil_copper',
+            C: 'kubejs:redstone_circuit' // Disabled should be below one
+            //C: 'immersiveengineering:component_electronic'
+        }
+    )
+
+    event.remove({ id: 'ae2:network/blocks/inscribers' })
+    event.shaped(
+        Item.of('ae2:inscriber'),
+        [
+            'APA',
+            'A A',
+            'ACA'
+        ],
+        {
+            A: '#c:plates/aluminum',
+            P: 'create:mechanical_press',
+            C: 'kubejs:redstone_circuit' // Disabled should be below one
+            //C: 'immersiveengineering:component_electronic'
+        }
+    )
+
+    event.replaceInput({ id: 'ae2:network/blocks/controller' }, 'ae2:smooth_sky_stone_block', 'abyssal_decor:moonsilver_ingot')
     
+    event.replaceInput({ id: 'ae2:network/blocks/storage_chest' }, 'minecraft:copper_ingot', 'immersiveengineering:component_electronic')
+
+    //endregion AE Machines
+
+    //region Components
+
+    event.replaceInput({ output: 'ae2:cell_component_1k' }, 'minecraft:redstone', 'abyssal_decor:moonsilver_ingot')
+    event.replaceInput({ output: 'ae2:cell_component_4k' }, 'minecraft:redstone', 'abyssal_decor:moonsilver_ingot')
+    event.replaceInput({ output: 'ae2:cell_component_16k' }, 'minecraft:glowstone_dust', 'abyssal_decor:moonsilver_ingot')
+    event.replaceInput({ output: 'ae2:cell_component_64k' }, 'minecraft:glowstone_dust', 'abyssal_decor:moonsilver_ingot')
+    event.replaceInput({ output: 'ae2:cell_component_256k' }, 'ae2:sky_dust', 'abyssal_decor:moonsilver_ingot')
+
+    event.replaceInput({ output: 'ae2:spatial_cell_component_2' }, 'minecraft:glowstone_dust', 'abyssal_decor:moonsilver_ingot')
+    event.replaceInput({ output: 'ae2:spatial_cell_component_16' }, 'minecraft:glowstone_dust', 'abyssal_decor:moonsilver_ingot')
+    event.replaceInput({ output: 'ae2:spatial_cell_component_128' }, 'minecraft:glowstone_dust', 'abyssal_decor:moonsilver_ingot')
+    
+    //endregion Components
+
 })
